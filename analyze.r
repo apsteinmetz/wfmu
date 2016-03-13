8748df00df6867f4fb8d6f43b4a2f991454d58cb
apsteinmetz <apsteinmetz@yahoo.com>
2016-03-13 16:07:07 -0400

    cleaned artist data and created artist tokens created docs of dj artist data

--- analyze.r
+++ analyze.r
@@ -8,53 +8,92 @@
 # 
 library(stringr)
 library(tm)
-library("plyr")
+library(dplyr)
 library("ggplot2")
 library("wordcloud")
 library("RColorBrewer")
 library("SnowballC")
-library(sentiment)
+#library(sentiment) not available
 
 
-load("allDJArtists.RData")
 
+cleanUpArtists<- function() {
 
-#needs work
-findLongestWord <- function(sentence) {
-  len <-0
-  wlen <- 0
-  longWord = ""
-  listOfWords = str_split(sentence," ")
-  #find longest word in a sentence
-  for (w in listOfWords) {
-    len <- str_length(w)
-    if (len >wlen) {
-      longWord=w
-      wlen <- len 
-    }
-  }
-  return(longWord)
+# one artist is all punctuation so give !!! special treatment
+  allDJArtists$artist<-str_replace(allDJArtists$artist,"!!!","chkchkchk")
+  # now change some common punctuation to space
+  #I'm sure there is a more elegant way to do this.
+  print("Stripping Punctuation")
+  allDJArtists$artist<-str_replace_all(allDJArtists$artist,"\\("," ")
+  allDJArtists$artist<-str_replace_all(allDJArtists$artist,"\\)"," ")
+  allDJArtists$artist<-str_replace_all(allDJArtists$artist,"\\*"," ")
+  allDJArtists$artist<-str_replace_all(allDJArtists$artist,'\\['," ")
+  allDJArtists$artist<-str_replace_all(allDJArtists$artist,'\\]'," ")
+  allDJArtists$artist<-str_replace_all(allDJArtists$artist,'[!:"/+-,]'," ")
+  allDJArtists$artist<-str_replace_all(allDJArtists$artist,"\\'"," ")
+  allDJArtists$artist<-str_replace_all(allDJArtists$artist,"\\&"," ")
+  allDJArtists$artist<-str_replace_all(allDJArtists$artist,"\\."," ")
+  allDJArtists$artist<-str_replace_all(allDJArtists$artist,"\\#"," ")
+  allDJArtists$artist<-str_trim(allDJArtists$artist)
+  #did we create any null entries
+  allDJArtists<-filter(allDJArtists,artist!="")
+  
+  #now some connecting words that might be spelled/used variantly
+  # first make all lower cast
+  print("Stripping ambiguous words")
+  allDJArtists$artist<-str_to_lower(allDJArtists$artist)
+  joinWords <- c("with","feat","featuring","vs","versus","and","the")
+  for (w in joinWords){
+    allDJArtists$artist<-str_replace_all(allDJArtists$artist,w," ")
+  }  
+
 }
 
-#expiriment with different algorithms to uniquely identify artist
-# one artist is all punctuation so give !!! special treatment
-allDJArtists$artist<-str_replace(allDJArtists$artist,"!!!","chkchkchk")
-# now change some common punctuation to space
-allDJArtists$artist<-str_replace_all(allDJArtists$artist,"\\("," ")
-allDJArtists$artist<-str_replace_all(allDJArtists$artist,"\\)"," ")
-allDJArtists$artist<-str_replace_all(allDJArtists$artist,"\\*"," ")
-allDJArtists$artist<-str_replace_all(allDJArtists$artist,"\\*"," ")
-allDJArtists$artist<-str_replace_all(allDJArtists$artist,'\\['," ")
-allDJArtists$artist<-str_replace_all(allDJArtists$artist,'\\]'," ")
-allDJArtists$artist<-str_replace_all(allDJArtists$artist,'[!:"/+-]'," ")
-allDJArtists$artist<-str_replace_all(allDJArtists$artist,'[!:"/+-]'," ")
 
-# use longest word in artist name as having the most info content
+
+combineArtistWords <- function(){
+  # we replaced all punctuation with spaces
+  #maybe strip spaces and combine all artist Words
+  artistToken1<-str_replace_all(allDJArtists$artist," ","")
+}
+
+combineTwoArtistWords <- function(){
+  # we replaced all punctuation with spaces
+  #maybe strip spaces and combine all artist Words
+  #combine first two words
+  print("Trying to make sense of artist names")
+  t<-str_split_fixed(allDJArtists$artist,pattern="[ ]+",n=3)[,1:2]
+  allDJArtists$artistToken2<-apply(t,MARGIN=1,FUN=paste,collapse="")
+}
+
+combineAllArtists <- function(){
+  t<- data.frame()
+  for (dj in levels(allDJArtists$DJ)){
+    #put all words in string for each DJ
+    print("Creating artist documents")
+  print(dj)  
+  t<-rbind(t,data.frame(DJ=dj,artists= allDJArtists%>%
+                          filter(DJ==dj)%>%
+                          select(artistToken2)%>% 
+                          unlist()%>%paste(collapse=" ")))
+             }
+  
+  return(t)
+}
+
+# ----------------- MAIN --------------
+load("allDJArtists.RData")
+cleanUpArtists()
+#expiriment with different algorithms to uniquely identify artist
+combineTwoArtistWords()
+
+#combine words into one document per DJ
+
+
 test<-filter(allDJArtists,DJ=="GK")
-test[1,2]
+djDocs<-combineAllArtists()
+
+# maybe use longest word in artist name as having the most info content
 findLongestWord(test$artist[1])
 
 sentence<-test$artist[1]
-
-#put all words in string for each DJ
-paste(test[,2],collapse=" ")
