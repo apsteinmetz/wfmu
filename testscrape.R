361088e73fb381e9df9ca243faac105cc4f699ab
apsteinmetz <apsteinmetz@yahoo.com>
2017-06-02 22:06:26 -0400

    more work

--- testscrape.R
+++ testscrape.R
@@ -2,38 +2,21 @@ testgetPlaylist <- function(plURLs,dj) {
   playlist = data.frame()
   i<-1
   print(paste(dj,i))
-  #assume playlist is a table.  scan all tables on page to find which is the correct one
-  
+
+  #extract table data of class 'song'
   wholepage <- read_html(paste(ROOT_URL, plURLs[i],sep=''))
-  plraw<-wholepage%>% html_nodes(xpath='//*[contains(concat( " ", @class, " " ), concat( " ", "song", " " ))]')%>%
+  plraw<-wholepage%>% html_nodes(css='.song')%>%
     html_text()%>%
     as.matrix()
   dim(plraw)<-c(5,length(plraw)/5)  
-  plraw<-plraw%>%t()%>%as.data.frame()
+  plraw<-plraw%>%t()%>%as_tibble()
   names(plraw)<-plraw[1,]
-  
+  plraw<-plraw[-1,]
+  plraw<-str_replace(plraw,'\n',"")
+  plraw<-apply(plraw,c(1,2),str_replace_all,'\\n',"")
+  plraw<-apply(plraw,c(1,2),str_trim)
+  pl<-as_tibble(plraw)
   pl<-data.frame()
-  for (n in 1:length(alltablerows)) {
-    if (length(html_nodes(alltables[n],'th')) > 0) {
-      pl<-alltables[[n]]%>%html_table(fill=TRUE)
-      } #we found the table with playlists
-  }
-  #TABLE STYLE 2
-  if (length(pl)==0){
-    pl<-alltables[[3]]%>%html_table(fill=T)
-    if (is.null(names(pl)) || names(pl)[1]=="X1") {
-      names(pl)<- pl[1,]
-      pl <- pl[-1,]
-    }
-  }  
-  #temp<-html(plURL[i])%>%html_nodes("table")%>%.[2]%>%html_table(fill=TRUE)
-  # try to correct tables without headers
-  #headers might be in second row
-  if (is.null(names(pl)) || names(pl)[1]=="X1") {
-    names(pl)<- pl[1,]
-    pl <- pl[-1,]
-    
-  }
   #exceptions we are aware of where table is nice but headers are not labeled "artist" ,"title"
   altTitleNames<-c('THE SONG','Track')
   altArtistNames<-c('THE STOOGE')
@@ -54,7 +37,7 @@ testgetPlaylist <- function(plURLs,dj) {
 #limit analysis to DJs with at least numShows shows and take the last numshows shows.
 numShows<-50
 # non-music shows
-excludeDJs<-c('SD','HA','BC')
+excludeDJs<-c('SD','HA','BC','AF','CP','HP','JP')
 djList<-filter(DJKey,showCount>numShows-1,!(DJ%in%excludeDJs)) %>%select(DJ) %>% .[,1]
 #djList<-filter(DJKey,showCount>numShows-1) %>%select(DJ) %>% .[,1]
 
