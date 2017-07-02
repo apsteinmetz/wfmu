0ff91b534be4226ed2df3a4654f3a44dabb4ea36
apsteinmetz <apsteinmetz@yahoo.com>
2017-07-02 15:41:07 -0400

    rebuilt to simplify. need to test

--- testscrape.R
+++ testscrape.R
@@ -1,7 +1,10 @@
 altArtistNames<-c('THE STOOGE','Band','Singer','Artist')
 altTitleNames<-c('THE SONG','Track','Song')
 altHeaderNames<-c(altArtistNames,altTitleNames)
-
+header_text_xpath<-paste("//tr[th='Track']",
+                          "or //tr[th='Title']",
+                          "or //tr[contains(th,'Song')]")
+                         "or //tr[contains(th,'SONG')]")
 fixHeaders<-function(pl){
   #takes a data frame
   names(pl)[names(pl)%in%altTitleNames]<-"Title"
@@ -23,17 +26,19 @@ handleBadTable<-function(wholepage){
     t() %>% 
     as_data_frame()
   plraw<-plraw[,!sapply(plraw,function(x) (all(x==""))),drop=F]
- # still has columns with only on-printable characters
-
-  #NOT WORKING
-
-}
+  # still has columns with only on-printable characters
   
+  #NOT WORKING
+  
+}
+
 #--------------------------------------------------------------------
 testgetPlaylist <- function(plURLs,dj) {
   i<-1
   print(paste(dj,i,plURLs[i,1]))
 
+  
+  
   wholepage <- read_html(paste(ROOT_URL, plURLs[i,1],sep=''))
   #try to pull out the show date.  assume first date in text on page is the show date
   airDate<-wholepage%>%
@@ -47,67 +52,92 @@ testgetPlaylist <- function(plURLs,dj) {
       as.Date("%d %B %Y")
     
   }
-  # are there rows with td of class=song?  get the table
-  if((wholepage%>%html_nodes(xpath="//td[@class='song']")%>%length())>0) {
-    plraw_head<-wholepage%>% 
-      html_nodes(xpath="//th[@class='song']") %>% 
-      html_text() %>%   
-      str_replace('\n','') %>% 
-      str_trim()
-      
-    plraw_row<-wholepage%>% 
-      html_nodes(xpath="//td[@class='song']") %>% 
-      html_nodes(xpath="//td[not(@colspan='7')]") #%>% 
-      plraw<-plraw_row %>% 
-      html_text() %>% 
-      str_replace('\n','') %>% 
-      str_trim() %>% 
-      matrix(ncol=length(plraw_head),byrow=T) %>% 
-      as_data_frame()
+  
+  plraw<- NULL
+  #simplest case. A table with obvious header names
+  #hope we have all variations on the song title label
+  if(wholepage%>%html_node(xpath=header_text_xpath)) {
+    plraw<-wholepage%>% 
+      html_node(xpath="//td[@class='song']/ancestor::table") %>%
+      html_table(fill=TRUE)
+  } else {
+    # are there rows in a table with td of class=song?  get the table
+    if(wholepage%>%html_node(xpath="//td[@class='song']") %>% length()>1) {
+      plraw<-wholepage %>% 
+        html_node(xpath="//td[@class='song']/ancestor::table") %>% 
+        html_table(fill=T)
+      #now find the row that has the header
+      for (n in 1:nrow(plraw)){
+        if (TRUE %in% (plraw[n,]%in%altHeaderNames)){
+          names(plraw)<-plraw[n,]
+          plraw<-plraw[n+1:nrow(plraw),]
+          break
+        }
+        if (n==nrow(plraw)) {
+          print("DUD. Can't find header")
+          plraw<-NULL #final dead end
+        }
+        
+      }
+    }
+  }
+  
+  if (is.null(plraw)){
+    # not an html table. now what? try to make a table after class song and finding header
+    if((wholepage%>%html_nodes(xpath="//td[@class='song']")%>%length())>0) {
+      plraw_head<-wholepage%>% 
+        html_nodes(xpath="//td[(@class='song') and not(@colspan | @align)]") %>% 
+        html_text() %>%   
+        str_replace('\n','') %>% 
+        str_trim()
 
+        plraw_row<-wholepage %>% 
+        html_nodes(xpath="//td[(@class='song') and not(@colspan | @align)]") %>% 
+        html_text() %>% 
+        str_replace('\n','') %>% 
+        str_trim() %>% 
+        matrix(ncol=length(plraw_head),byrow=T) %>% 
+        as_data_frame()
+      
       names(plraw)<-plraw_head
-    
-    if (ncol(plraw)>10) plraw<-handleBadTable(wholepage)
-  } else  {
-    #if not, does it have a table row with "artist | title | song | album".  assume first such table is playlist
-    if(wholepage%>%html_node(xpath="//tr[td='Artist'] or //tr[td='Album']")) {
-      plraw<-wholepage%>% 
-        html_node(xpath="//tr[td='Artist']/ancestor::table") %>%
-        html_table(fill=TRUE)
-    } else {
       plraw<-NULL #we got nothin'
     }
   }
   if(is.null(plraw)) {
-    playlist<-plraw
-    print('DUD')
-    } else{ # try one more thing
-      if (names(plraw)[1]=="X1"){
-        #scan until we find the playlist header
-        for (n in 1:nrow(plraw)){
-          if (plraw$X1[n]%in%altHeaderNames){
-            names(plraw)<-plraw[n,]
-            plraw<-plraw[n+1:nrow(plraw),]
-            break
-          }
-          
-          
+    print("DUD. Can't find a playlist.")
+  } else{ # try one more thing
+    if (names(plraw)[1]=="X1"){
+      #scan until we find the playlist header
+      for (n in 1:nrow(plraw)){
+        if (plraw$X1[n]%in%altHeaderNames){
+          names(plraw)<-plraw[n,]
+          plraw<-plraw[n+1:nrow(plraw),]
+          break
         }
       }
-      if(TRUE%in%is.na(names(plraw))) plraw<-plraw[,-which(is.na(names(plraw)))] #sometimes an NA column
-      
-      playlist<-plraw%>%
-        as_data_frame()%>%
-        fixHeaders() %>%
-        mutate(DJ=dj,AirDate=airDate)%>%
-        select(DJ,AirDate,Artist,Title)%>%
-        filter(Artist!='')
-      print(playlist[1:5,])
+      if (n==nrow(plraw)) {
+        print("DUD. Can't find header")
+        plraw<-NULL #final dead end
+      }
     }
-
-  return(playlist)
+  }
+# final clean up if we have something
+if (is.null(plraw)){ playlist<- NULL
+} else {
+  if(TRUE%in%is.na(names(plraw))) plraw<-plraw[,-which(is.na(names(plraw)))] #sometimes an NA column
+  
+  playlist<-plraw%>%
+    as_data_frame()%>%
+    fixHeaders() %>%
+    mutate(DJ=dj,AirDate=airDate)%>%
+    select(DJ,AirDate,Artist,Title)%>%
+    filter(Artist!='')
+  print(playlist[1:5,])
+  
 }
 
+return(playlist)
+}
 
 
 #-------------- MAIN -----------------
