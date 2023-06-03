7307165ab56c68dd70bf278ffaf51dd3599ff714
Art Steinmetz <apsteinmetz@yahoo.com>
2023-06-03 15:30:00 -0400

    erase stale files. add data.table versions of data

--- 4 - copy files to shiny side.R
+++ 4 - copy files to shiny side.R
@@ -1,7 +1,9 @@
+
 # copy precalculated data over to shiny app directory
 
-# load then save with old file format that works with R 3.4
+library(dtplyr)
 
+# load then save with old file format that works with R 3.4
 load(file = "data/DJKey.RData")
 load(file = "data/playlists.Rdata")
 load(file = "data/djSimilarity.RData")
@@ -12,9 +14,14 @@ save(playlists,file = "data/playlists.Rdata",version = 2)
 save(dj_similarity_tidy,file = "data/djSimilarity.RData",version = 2)
 save(djdtm,file = "data/djdtm.RData",version = 2)
 
+DJKey_dt <- lazy_dt(DJKey)
+playlists_dt <- lazy_dt(playlists)
+dj_similarity_dt <- lazy_dt(dj_similarity_tidy)
 
-
-
+save(DJKey_dt,file = "data/DJKey_dt.RData",version = 2)
+save(playlists_dt,file = "data/playlists_dt.Rdata",version = 2)
+save(dj_similarity_dt,file = "data/djSimilarity_dt.RData",version = 2)
+save(djdtm,file = "data/djdtm.RData",version = 2)
 
 library(fs)
 
@@ -22,5 +29,10 @@ files = c("DJKey.rdata",
           "djdtm.rdata",
           "playlists.rdata",
           "djsimilarity.rdata")
+files_dt = c("DJKey_dt.rdata",
+          "djdtm.rdata",
+          "playlists_dt.rdata",
+          "djsimilarity_dt.rdata")
 shiny_app_dir = "../wfmu_explorer/"
 file_copy(paste0("data/",files),paste0(shiny_app_dir,files),overwrite = TRUE)
+file_copy(paste0("data/",files_dt),paste0(shiny_app_dir,files_dt),overwrite = TRUE)
